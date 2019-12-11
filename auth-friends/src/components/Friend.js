a93aab69aa2a7b9f21a86c64617a4e9ddf929d5a
CodingCCarpenter <christineassists@aol.com>
2019-12-10 20:28:33 -0600

    final commit before grading

--- auth-friends/src/components/Friend.js
+++ auth-friends/src/components/Friend.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Friend = (props) => {
     return (
-        <div>
+        <div className='friend'>
             <h2>{props.name}</h2>
             <p>{props.age} years old</p>
             <p><em>{props.email}</em></p>
